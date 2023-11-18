dfec2516f42f6265f91582439adc2ea9ded83153
minsuh1004 <immsk812@gmail.com>
2023-11-18 05:08:36 -0800

    Added SMS functionality (shows in messaging app)

--- app/src/main/java/edu/uw/ischool/msk812/awty/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/msk812/awty/MainActivity.kt
@@ -1,16 +1,21 @@
 package edu.uw.ischool.msk812.awty
 
+import android.Manifest
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telephony.SmsManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 const val ALARM_ACTION = "edu.uw.ischool.msk812.ALARM"
 
@@ -20,6 +25,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var phoneText : EditText
     lateinit var minutesText : EditText
     var receiver : BroadcastReceiver? = null
+    val smsPermisionCode = 812
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,62 +36,50 @@ class MainActivity : AppCompatActivity() {
         minutesText = findViewById(R.id.minutesNag)
 
 
-        //val intent = Intent(this, MessageReceiver::class.java)
-        //val intent = Intent(ALARM_ACTION)
-
-        //val alarmManager : AlarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-
-
         startBtn.setOnClickListener {
             if (startBtn.text == "Start") {
                 if (messageText.text.isNotEmpty() && phoneText.text.length == 10 &&
                     minutesText.text.isNotBlank() && minutesText.text[0] != '0') {
                     Toast.makeText(this, "Started alarm", Toast.LENGTH_SHORT).show()
-                    /*intent.putExtra("message", messageText.text.toString())
-                    intent.putExtra("phoneNum", phoneText.text.toString())
-                    val pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-                    val calcMinutes = minutesText.text.toString().toInt() * 60 * 1000
-                    alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), calcMinutes.toLong(), pendingIntent)
-                    startBtn.text = "Stop"*/
                     receiveMessage()
                 }
             } else {
                 Toast.makeText(this, "Stopped alarm", Toast.LENGTH_SHORT).show()
-                /*val pendingIntent = PendingIntent.getBroadcast(this, 0, intent,
-                    PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_IMMUTABLE)
-                alarmManager.cancel(pendingIntent)
-                startBtn.text = "Start"*/
-                cancelAlarm()
+                cancelMessage()
             }
         }
     }
-
 
     fun receiveMessage() {
         val activityThis = this
+        val smsManager = SmsManager.getDefault()
         val message = messageText.text.toString()
         val phoneNum = phoneText.text.toString()
-        val calcMinutes = minutesText.text.toString().toInt() * 60 * 1000
+        val calcMinutes = minutesText.text.toString().toLong() * 60 * 1000
+        val phoneNumFormat = "(${phoneNum.substring(0, 3)}) ${phoneNum.substring(3, 6)}-${phoneNum.substring(6, 10)}"
 
-        receiver = object : BroadcastReceiver() {
-            override fun onReceive(context: Context?, intent: Intent?) {
-                val phoneNumFormat = "(${phoneNum.substring(0, 3)}) ${phoneNum.substring(3, 6)}-${phoneNum.substring(6, 10)}"
-                val fullMessage = "${phoneNumFormat}: $message"
-                Toast.makeText(activityThis, fullMessage, Toast.LENGTH_SHORT).show()
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.SEND_SMS), smsPermisionCode)
+        } else {
+            receiver = object : BroadcastReceiver() {
+                override fun onReceive(context: Context?, intent: Intent?) {
+                    smsManager.sendTextMessage(phoneNumFormat, null, message, null, null)
+                }
             }
+
+            val filter = IntentFilter(ALARM_ACTION)
+            registerReceiver(receiver, filter)
+
+            val intent = Intent(ALARM_ACTION)
+            val pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+            val alarmManager : AlarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), calcMinutes, pendingIntent)
+            startBtn.text = "Stop"
         }
-        val filter = IntentFilter(ALARM_ACTION)
-        registerReceiver(receiver, filter)
-
-        val intent = Intent(ALARM_ACTION)
-        val pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
-
-        val alarmManager : AlarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), calcMinutes.toLong(), pendingIntent)
-        startBtn.text = "Stop"
     }
 
-    fun cancelAlarm() {
+    fun cancelMessage() {
         val intent = Intent(ALARM_ACTION)
         val pendingIntent = PendingIntent.getBroadcast(this, 0, intent,
             PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_IMMUTABLE)
@@ -97,13 +91,22 @@ class MainActivity : AppCompatActivity() {
         startBtn.text = "Start"
     }
 
-    /*class MessageReceiver : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
-            val message = intent!!.getStringExtra("message")
-            val phoneNum = intent!!.getStringExtra("phoneNum")
-            val phoneNumFormat = "(${phoneNum!!.substring(0, 3)}) ${phoneNum.substring(3, 6)}-${phoneNum.substring(6, 10)}"
-            val fullMessage = "${phoneNumFormat}: $message"
-            Toast.makeText(context, fullMessage, Toast.LENGTH_SHORT).show()
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            smsPermisionCode -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (minutesText.text.isNotBlank() && minutesText.text[0] != '0') {
+                        receiveMessage()
+                    }
+                } else {
+                    Toast.makeText(this, "Permission denied. SMS cannot be sent.", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
-    }*/
+    }
 }
